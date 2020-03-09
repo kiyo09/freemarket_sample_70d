5b4c69d0572b4e1ee53a5c5d4ff45f54ecbff1d5
kazuto0291 <zutozuto03170291@gmail.com>
2020-03-09 10:39:20 +0900

    routes.rbの記述変更
    7行目のresources :users, only: [:show]の記述があるので、
    
    devise_for :users・’get 'users/show'の削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :items
