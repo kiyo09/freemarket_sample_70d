85ab742f3c7234f435784fc48980c886f2ffa345
kazuto0291 <zutozuto03170291@gmail.com>
2020-03-11 13:37:59 +0900

    create-actionの編集とルーティングの編集とビューファイル作成と
    ルーティング
    参照するコントローラーを変更registrationのみ
    個人情報を登録するルーティングを設定
    コントローラー
    バリデーションチェック
    sessionに保持させる

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @user_details = @user.build_user_detail
+    render :new_user_detail
+  end
 
   # GET /resource/edit
   # def edit
@@ -38,12 +46,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
