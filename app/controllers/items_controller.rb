ad461a3e9a42aa24da5174caf0d3fcd791c7f029
okcysk018 <a13.gh38@gmail.com>
2020-03-13 12:45:04 +0900

    itemsへの機能追加
    既存のページを微修正
    以下のアクションを追加実装
    pay：購入
    purchase：購入確認
    done：購入完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
 
+  require 'payjp'
+
   def index
     @items = Item.includes(:user)
   end
@@ -16,6 +18,40 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  # 購入確認
+  def purchase
+    @item = Item.find(params[:id])
+    # FIXME: itemテーブルから画像取得
+    # @image = @item.images.first
+    card = CreditCard.where(user_id: current_user.id).first
+    if card.present?
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @customer_card = customer.cards.retrieve(card.card_id)
+    end
+  end
+
+ # 購入完了
+  def done
+  end
+
+  # 購入
+  def pay
+    card = CreditCard.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to controller: "credit_cards", action: "new"
+    else
+      @item = Item.find(params[:id])
+      Payjp.api_key = Rails.application.credentials.PAYJP_SECRET_KEY
+      charge = Payjp::Charge.create(
+      amount: @item.price,
+      customer: card.customer_id,
+      currency: 'jpy'
+      )
+      Item.where(id: @item.id).update_all(buyer_id: current_user.id)
+      redirect_to action: 'done'
+    end
+  end
+
   def destroy
     item = Item.find(params[:id])
     item.destroy
@@ -29,7 +65,6 @@ class ItemsController < ApplicationController
     item = Item.find(params[:id])
     item.update(item_params)
   end
-
 
   private
   def item_params
