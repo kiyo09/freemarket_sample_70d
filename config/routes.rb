8eba3c948d0f7342908978a78e8293579eae5600
okcysk018 <a13.gh38@gmail.com>
2020-03-13 15:46:05 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
-  resources :items
+  resources :items do
+    collection do
+      get  'purchase/:id'=>  'items#purchase', as: 'purchase'
+      post 'pay/:id'=>   'items#pay', as: 'pay'
+      get  'done'=>      'items#done', as: 'done'
+    end
+  end
   resources :users, only: [:show]
+  resources :credit_cards, only: [:new, :index, :create, :destroy]
   resources :payments, only: [:new]
 end
