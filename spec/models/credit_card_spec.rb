5d364a808ae0bfd8a56fb5d3ba49c39e7f1ff452
okcysk018 <a13.gh38@gmail.com>
2020-03-13 15:35:49 +0900

    クレカ登録単体テストコード
    クレジットカードモデルのバリデーションテストのみ

---
+++ spec/models/credit_card_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe CreditCard do
+  describe '#create' do
+    it "is invalid without an user_id" do
+      creditCard = build(:creditCard, user_id: nil)
+      
+      creditCard.valid?
+      expect(creditCard.errors[:user_id]).to include("can't be blank")
+    end
+
+    it "is invalid without a customer_id" do
+      creditCard = build(:creditCard, customer_id: nil)
+      creditCard.valid?
+      expect(creditCard.errors[:customer_id]).to include("can't be blank")
+    end
+
+    it "is invalid without a card_id" do
+      creditCard = build(:creditCard, card_id: nil)
+      creditCard.valid?
+      expect(creditCard.errors[:card_id]).to include("can't be blank")
+    end
+  end
+end
