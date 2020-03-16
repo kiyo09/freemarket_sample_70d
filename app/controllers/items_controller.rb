48e4e645ee7f33e775d4f2f4477f5d05e3158aaa
kiyo09 <60495508+kiyo09@users.noreply.github.com>
2020-03-16 12:27:05 +0900

    多階層カテゴリーの登録機能
    出品時孫カテゴリーまで登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,18 @@
 class ItemsController < ApplicationController
-  before_action :set_item, except: [:index, :new, :create]
-
+  before_action :set_item, only: [:show, :edit, :destroy, :update]
+  before_action :set_category, except: [:create, :destroy, :category_grandchildren]
+  
   def index
     @items = Item.includes(:user).limit(6)
-
-    # @image = Image.find(item_id).first
+    @parents = Category.where(ancestry: nil)
   end
 
   def new
     @item = Item.new
+    @category_parent_array = ["---"]
+      Category.where(ancestry: nil).each do |parent|
+         @category_parent_array << parent.name
+      end
     @item.images.build
     @item.build_brand
     @category = Category.all.order("id ASC").limit(13)
@@ -38,6 +42,25 @@ class ItemsController < ApplicationController
     item.update(item_params)
   end
 
+  def search
+    respond_to do |format|
+      format.html
+      format.json do
+       @children = Category.find(params[:parent_id]).children
+       #親ボックスのidから子ボックスのidの配列を作成してインスタンス変数で定義
+      end
+    end
+  end
+  
+  def category_children  
+    @category_children = Category.find_by(name: "#{params[:productcategory]}").children 
+  end
+  # Ajax通信で送られてきたデータをparamsで受け取り､childrenで子を取得
+
+  def category_grandchildren
+    @category_grandchildren = Category.find("#{params[:productcategory]}").children
+  end
+
 
   private
   def item_params
@@ -63,5 +86,10 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
+  def set_category
+    @category = Category.all.order("id ASC").limit(13)
+  end
+
 
 end
