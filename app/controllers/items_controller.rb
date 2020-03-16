712fbca3d3197e94df4404acb06bcc3298ddd7f1
kiyo09 <60495508+kiyo09@users.noreply.github.com>
2020-03-16 18:29:44 +0900

    エラー修正
    beforeアクションの修正で購入可能に

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :destroy, :update]
+  before_action :set_item, only: [:show, :edit, :destroy, :update, :purchase]
   before_action :set_category, except: [:create, :destroy, :category_grandchildren]
 
   require 'payjp'
