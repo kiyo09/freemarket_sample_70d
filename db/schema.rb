b4438c32aff0321cff522d7de7ebbb17c0d3d88a
EgawaTomohide <sjiangchuan189@gmail.com>
2020-03-17 17:20:13 +0900

    新規登録

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2020_03_10_114818) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
