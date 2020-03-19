767b630fc0d0c6b946c6da233b130d35c3fbe586
kiyo09 <60495508+kiyo09@users.noreply.github.com>
2020-03-19 10:55:43 +0900

    商品出品（複数画像、販売利益、手数料計算）
    商品出品時、画像10枚プレビュー
    価格設定時、利益、手数料の表示

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,253 +1,205 @@
-// プレビュー機能
-  //'change'イベントでは$(this)で要素が取得できないため、 'click'イベントを入れた。
-  //これにより$(this)で'input'を取得することができ、inputの親要素である'li'まで辿れる。
+// ===============================================================
+// 出品ページ
+// 販売手数料、販売利益
+// ===============================================================
 
-  $(document).on('click', '.image_upload', function(){
-    //inputの要素はクリックされておらず、inputの親要素であるdivが押されている。
-    //だからdivのクラス名をclickした時にイベントが作動。
-    //div（this）から要素を辿ればinputを指定することが可能。
+$(function(){
+  $("#item_price").change(function(){
+    var price = $('#item_price').val();
+    var priceI = parseInt(price);
+    var fee = (priceI / 10);
+    var profit = (priceI - fee);
+    $(".fee-span").append(fee);
+    $(".profit-span").append(profit);
+  })
+})
 
-    //$liに追加するためのプレビュー画面のHTML。
-    // var preview = $('<div class="image-preview__wapper"><img class="preview"></div><div class="image-preview_btn"><div class="image-preview_btn_delete">削除</div></div>'); 
-    //次の画像を読み込むためのinput。 
-    // var append_input = $(`<li class="input"><label class="upload-label"><div class="upload-label__text"><i class="fa fa-camera"></i><div class="input-area"><input name="item[images_attributes][#{@item.images.count}][src]",class="hidden image_upload" type="file"></div></div></label></li>`)
+// ===============================================================
+// 出品ページ
+// 複数画像出品
+// ===============================================================
 
-    const append_input = (index)=> {
-      const html = `<li data-index="${index}" class="input">
-                      <div class="upload-label__text">
-                        <i class="fa fa-camera"></i>
-                        <div class="input-area">
-                          <input name="item[images_attributes][${index}][image]",class="hidden image_upload" type="file">
-                        </div>
-                      </div>
-                    </li>`
-      return html;
-    }
+$(function () {
+  var dropzone = $('.dropzone-area');
+  var dropzone2 = $('.dropzone-area2');
+  var images = [];
+  var inputs = [];
+  var input_area = $('.input_area');
+  var preview = $('#preview');
+  var preview2 = $('#preview2');
 
-    const preview = (index, url)=> {
-      const html = `<div class="image-preview__wapper">
-                      <img data-index="${index}" src="${url}" class="preview">
-                    </div>
-                    <div class="image-preview_btn">
-                      <div class="image-preview_btn_delete">削除</div>
-                    </div>` 
-      return html;
-    }
-    
-
-  //   const buildImg = (index, url)=> {
-  //   const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
-  //   return html;
-  // }
-
-
-
-    $ul = $('#previews')
-    $li = $(this).parents('li');
-    $label = $(this).parents('.upload-label');
-    $inputs = $ul.find('.image_upload');
-    //inputに画像を読み込んだら、"プレビューの追加"と"新しいli追加"処理が動く
-    $('.image_upload').on('change', function (e) {
-      //inputで選択した画像を読み込む
-      var reader = new FileReader();
-
-
-      // プレビューに追加させるために、inputから画像ファイルを読み込む。
-      reader.readAsDataURL(e.target.files[0]);
-
-      //画像ファイルが読み込んだら、処理が実行される。 
-      reader.onload = function(e){
-        //previewをappendで追加する前に、プレビューできるようにinputで選択した画像を<img>に'src'で付与させる
-        // つまり、<img>タグに画像を追加させる
-        $(preview).find('.preview').attr('src', e.target.result);
-      }
-
-      //inputの画像を付与した,previewを$liに追加。
-      $li.append(preview);
-
-      //プレビュー完了後は、inputを非表示にさせる。これによりプレビューだけが残る。
-      $label.css('display','none'); // inputを非表示
-      $li.removeClass('input');     // inputのクラスはjQueryで数を数える時に邪魔なので除去
-      $li.addClass('image-preview'); // inputのクラスからプレビュー用のクラスに変更した
-      $lis = $ul.find('.image-preview'); // クラス変更が完了したところで、プレビューの数を数える。 
-      $('#previews li').css({
-        'width': `128px`,
-        'height': `128px`
-      })
-
-
-
-
-      //"ul"に新しい"li(inputボタン)"を追加させる。
-      if($lis.length <= 4 ){
-        $ul.append(append_input)
-        $('#previews li:last-child').css({
-          'width': `calc(100% - (20% * ${$lis.length}))`
-        })
-      }
-      else if($lis.length == 5 ){
-        $li.addClass('image-preview');
-        $ul.append(append_input)
-        $('#previews li:last-child').css({
-          'width': `100%`
-        })
-      }
-      // ９個のプレビューのとき、1個のinputを追加。最後の数は9です。
-      else if($lis.length <= 9 ){
-        $li.addClass('image-preview');
-        $ul.append(append_input)
-        $('#previews li:last-child').css({
-          'width': `calc(100% - (20% * (${$lis.length} - 5 )))`
-        })
-      }
-      //inputの最後の"data-image"を取得して、input nameの番号を更新させてる。
-      // これをしないと、それぞれのinputの区別ができず、最後の1枚しかDBに保存されません。
-      // 全部のプレビューの番号を更新することで、プレビューを削除して、新しく追加しても番号が1,2,3,4,5,6と綺麗に揃う。だから全部の番号を更新させる
-      $inputs.each( function( num, input ){
-        //nameの番号を更新するために、現在の番号を除去
-        $(input).removeAttr('name');
-        $(input).attr({
-          name:"product[images_attributes][" + num + "][name]",
-          id:"product_images_attributes_" + num + "_name"
-        });
-      });
-
+  $(document).on('change', 'input[type= "file"].upload-image', function (event) {
+  var file = $(this).prop('files')[0];
+  var reader = new FileReader();
+  inputs.push($(this));
+  var image = $(`<div class= "img_view"><img></div>`);
+  reader.onload = function (e) {
+    var btn_wrapper = $('<div class="btn_wrapper"><div class="delete-img-btn">削除</div></div>');
+    image.append(btn_wrapper);
+    image.find('img').attr({
+      src: e.target.result
+    })
+  }
+  reader.readAsDataURL(file);
+  images.push(image);
+  if (images.length <= 4) {
+    $('#preview').empty();
+    $.each(images, function (index, image) {
+      image.data('image', index);
+      preview.append(image);
+    })
+    dropzone.css({
+      'width': `calc(100% - (20% * ${images.length}))`
+    })
+    // 画像が５枚のとき１段目の枠を消し、２段目の枠を出す
+  } else if (images.length == 5) {
+    $("#preview").empty();
+    $.each(images, function (index, image) {
+      image.data("image", index);
+      preview.append(image);
     });
+    dropzone2.css({
+      display: "block"
+    });
+    dropzone.css({
+      display: "none"
+    });
+    preview2.empty();
+
+    // 画像が６枚以上のとき
+  } else if (images.length >= 6) {
+    // １〜５枚目の画像を抽出
+    var pickup_images1 = images.slice(0, 5);
+
+    // １〜５枚目を１段目に表示
+    $('#preview').empty();
+    $.each(pickup_images1, function (index, image) {
+      image.data('image', index);
+      preview.append(image);
+    })
+
+    // ６枚目以降の画像を抽出
+    var pickup_images2 = images.slice(5);
+
+    // ６枚目以降を２段目に表示
+    $.each(pickup_images2, function (index, image) {
+      image.data('image', index + 5);
+      preview2.append(image);
+    })
+
+    dropzone.css({
+      'display': 'none'
+    })
+    dropzone2.css({
+      'display': 'block',
+      'width': `calc(100% - (20% * ${images.length - 5}))`
+    })
+
+    // 画像が１０枚になったら枠を消す
+    if (images.length == 10) {
+      dropzone2.css({
+        display: "none"
+      });
+    }
+  }
+  var new_image = $(`<input name="item[images_attributes][${images.length}][image]" class="upload-image" data-image= ${images.length} type="file" id="upload-image">`);
+  input_area.prepend(new_image);
+  $.each(input_area.children(), function (index, input) {
+  })
   });
-
-  //削除ボタンをクリックしたとき、処理が動く。
-$(document).on('click','.image-preview_btn_delete',function(){
-  var append_input = $(`<li class="input"><label class="upload-label"><div class="upload-label__text">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード<div class="input-area"><input class="hidden image_upload" type="file"></div></div></label></li>`)
-  $ul = $('#previews')
-  $lis = $ul.find('.image-preview');
-  $input = $ul.find('.input');
-  $ul = $('#previews')
-  $li = $(this).parents('.image-preview');
-
-
-  //"li"ごと削除して、previewとinputを削除させる。
-  $li.remove();
-
-  // inputボタンのサイズを更新する、または追加させる
-  // まずはプレビューの数を数える。
-  $lis = $ul.find('.image-preview');
-  $label = $ul.find('.input');
-  if($lis.length <= 4 ){
-    // inputのサイズを変更
-    $('#previews li:last-child').css({
-      'width': `calc(100% - (20% * ${$lis.length}))`
+  $(document).on('click', '.delete-img-btn', function () {
+  var target_image = $(this).parent().parent();
+  $.each(input_area.children(), function (index, input) {
+    if (input.dataset.image == target_image.data('image')) {
+      input.remove();
+      target_image.remove();
+      var num = input.dataset.image
+      images.splice(num, 1);
+      inputs.splice(num, 1);
+      if (inputs.length == 0) {
+        $('input[type= "file"].upload-image').attr({
+          'data-image': 0
+        })
+      }
+    }
+  })
+  $.each(input_area.children(), function (index, input) {
+    input.dataset.image = index
+  })
+  $('input[type= "file"].upload-image:first').attr({
+    'data-image': inputs.length
+  })
+  $.each(inputs, function (index, input) {
+    var input = $(this)
+    input.attr({
+      'data-image': index
     })
-  }
-  else if($lis.length == 5 ){
-    // inputのサイズを変更
-    $('#previews li:last-child').css({
-      'width': `100%`
+    $('input[type= "file"].upload-image:first').after(input)
+  })
+  if (images.length <= 4) {
+    $('#preview').empty();
+    $.each(images, function (index, image) {
+      image.data('image', index);
+      preview.append(image);
     })
-  }
-  else if($lis.length < 9 ){
-    // inputのサイズを変更
-    $('#previews li:last-child').css({
-      'width': `calc(100% - (20% * (${$lis.length} - 5 )))`
+    dropzone.css({
+      display: "block",
+      width: `calc(100% - (20% * ${images.length}))`
+
     })
-  }
-  else if($lis.length == 9 ){
-    $ul.append(append_input) // 9個の時だけ、新しいinputを追加してやる
-    $('#previews li:last-child').css({
-      'width': `calc(100% - (20% * (${$lis.length} - 5 )))`
+    dropzone2.css({
+      display: "none"
+    });
+    // 画像が５枚のとき１段目の枠を消し、２段目の枠を出す
+  } else if (images.length == 5) {
+    $("#preview").empty();
+    $.each(images, function (index, image) {
+      image.data("image", index);
+      preview.append(image);
+    });
+    dropzone2.css({
+      display: "block",
+      width: '100%'
+    });
+    dropzone.css({
+      display: "none"
+    });
+    preview2.empty();
+
+    // 画像が６枚以上のとき
+  } else if (images.length >= 6) {
+    // １〜５枚目の画像を抽出
+    var pickup_images1 = images.slice(0, 5);
+
+    // １〜５枚目を１段目に表示
+    $('#preview').empty();
+    $.each(pickup_images1, function (index, image) {
+      image.data('image', index);
+      preview.append(image);
     })
-  }
+
+    // ６枚目以降の画像を抽出
+    var pickup_images2 = images.slice(5);
+
+    // ６枚目以降を２段目に表示
+    $.each(pickup_images2, function (index, image) {
+      image.data('image', index + 5);
+      preview2.append(image);
+    })
+    dropzone.css({
+      'display': 'none'
+    })
+    dropzone2.css({
+      'display': 'block',
+      'width': `calc(100% - (20% * ${images.length - 5}))`
+    })
+
+    // 画像が１０枚になったら枠を消す
+    if (images.length == 10) {
+      dropzone2.css({
+        display: "none"
+      });
+     }
+   }
+  })
 });
-
-
-
-
-
-// $(function(){
-//   //DataTransferオブジェクトで、データを格納する箱を作る
-//   var dataBox = new DataTransfer();
-//   //querySelectorでfile_fieldを取得
-//   var file_field = document.querySelector('input[type=file]')
-//   //fileが選択された時に発火するイベント
-//   $('#img-file').change(function(){
-//     //選択したfileのオブジェクトをpropで取得
-//     var files = $('input[type="file"]').prop('files')[0];
-//     $.each(this.files, function(i, file){
-//     //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
-//     var fileReader = new FileReader();
-
-//     //DataTransferオブジェクトに対して、fileを追加
-//     dataBox.items.add(file)
-//     //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
-//     file_field.files = dataBox.files
-
-//  var num = $('.item-image').length + 1 + i
-//       fileReader.readAsDataURL(file);
-// 　　　 //画像が10枚になったら超えたらドロップボックスを削除する
-//       if (num == 10){
-//         $('.exmain-image__input').css('display', 'none')   
-//       }
-//       //読み込みが完了すると、srcにfileのURLを格納
-//       fileReader.onloadend = function() {
-//         var src = fileReader.result
-//         var html= `<div class='item-image' data-image="${file.name}">
-//                     <div class=' item-image__content'>
-//                       <div class='item-image__content--icon'>
-//                         <img src=${src} width="114" height="80" >
-//                       </div>
-//                     </div>
-//                     <div class='item-image__operetion'>
-//                       <div class='item-image__operetion--delete'>削除</div>
-//                     </div>
-//                   </div>`
-//         //image_box__container要素の前にhtmlを差し込む
-//       $('.exmain-image__input').before(html);
-//     };
-//     //image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変えてやる。
-//     $('.exmain-image__input').attr('class', `item-num-${num}`)
-//     });
-//   });
-// });
-
-// $(document).on("click", '.item-image__operetion--delete', function(){
-//   //プレビュー要素を取得
-//   var target_image = $(this).parent().parent()
-//   //プレビューを削除
-//   target_image.remove();
-//   //inputタグに入ったファイルを削除
-//   file_field.val("")
-// })
-
-
-
-
-
-
-// $(document).on('turbolinks:load', ()=> {
-//   // 画像用のinputを生成する関数
-//   const buildFileField = (index)=> {
-//     const html = `<div data-index="${index}" class="js-file_group">
-//                     <input class="js-file" type="file"
-//                     name="item[images_attributes][${index}][image]"
-//                     id="item_images_attributes_${index}_image"><br>
-//                     <div class="js-remove">削除</div>
-//                   </div>`;
-//     return html;
-//   }
-
-//   // file_fieldのnameに動的なindexをつける為の配列
-//   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-
-//   $('.exmain-image__input').on('change', '.js-file', function(e) {
-//     // fileIndexの先頭の数字を使ってinputを作る
-//     $('.exmain-image__input').append(buildFileField(fileIndex[0]));
-//     fileIndex.shift();
-//     // 末尾の数に1足した数を追加する
-//     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-//   });
-
-//   $('.exmain-image__input').on('click', '.js-remove', function() {
-//     $(this).parent().remove();
-//     // 画像入力欄が0個にならないようにしておく
-//     if ($('.js-file').length == 0) $('.exmain-image__input').append(buildFileField(fileIndex[0]));
-//   });
-// });
