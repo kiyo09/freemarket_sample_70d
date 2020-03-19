577219ad8f8f8f01cfea6f439faf2de0e294c555
okcysk018 <a13.gh38@gmail.com>
2020-03-19 13:06:22 +0900

    微修正
    item.jsのターボリンクスを非活動に

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -84,6 +84,11 @@ class ItemsController < ApplicationController
 
   def update
     @item.update(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def search
