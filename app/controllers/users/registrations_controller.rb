a99c64aadbb8ab711d68c82a4d7a22d26fbd7804
kazuto0291 <zutozuto03170291@gmail.com>
2020-03-20 18:42:23 +0900

    flash-message-haml-scss-setting

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -26,7 +26,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create_user_detail
     @user = User.new(session["devise.regist_data"]["user"])
     @user_detail = UserDetail.new(user_detail_params)
-    
+    # binding.pry
     unless @user_detail.valid?
       flash.now[:alert] = @user_detail.errors.full_messages
       # render :new and return
