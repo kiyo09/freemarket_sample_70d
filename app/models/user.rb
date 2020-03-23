570fc963ba62dcc5820f9473e8d053862f67095e
kazuto0291 <zutozuto03170291@gmail.com>
2020-03-23 13:34:37 +0900

    association-fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :items
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, omniauth_providers: [:facebook, :google_oauth2]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   has_one :credit_card, dependent: :destroy
   has_one :user_detail, dependent: :destroy
   has_many :sns_credentials
